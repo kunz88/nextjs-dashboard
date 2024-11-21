d8e09ce17f4d6149e720cc595a0d0e7d7a8603d7
kunz88 <cunsolostefano@yahoo.it>
2024-11-21 17:56:31 +0100

    add : dashboard page

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -44,7 +44,7 @@ const { yAxisLabels, topLabel } = generateYAxis(revenue);
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
-                className="w-full rounded-md bg-blue-300"
+                className="w-full rounded-md bg-blue-900"
                 style={{
                   height: `${(chartHeight / topLabel) * month.revenue}px`,
                 }}
